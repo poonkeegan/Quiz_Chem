5aeb4e3756c0431120432933588e9777c171ad09
temeyun <poonkeegan@gmail.com>
2015-01-31 14:38:32 -0500

    Finished List
    Added all 118 elements.

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.io.*;
 public class Main {
 
 	public static void main(String[] args) {
-		String [] values = {"atomic number", "chemical element", "name"};
+		String [] values = {"name", "chemical element","atomic number" };
 		String [][] table;
 		Scanner scanner;
 		File list = new File("list.txt");
@@ -29,12 +29,13 @@ public class Main {
 			//setup table array values
 			table = new String[fileLength][3];
 			for(int i = 0; i < fileLength; i++){
-				table[i] = scanner.nextLine().split(" ");
+				table[i] = scanner.nextLine().split("	");
 			}
 			
 			//generate random questions
 			boolean run = true;
 			while(run){
+				
 				//figure out which value to quiz on
 				int randNum = (int)(Math.random()*3.0);
 				//which value to present
@@ -43,6 +44,7 @@ public class Main {
 					randNumTwo = (int)(Math.random()*3.0);
 				//which element to quiz on
 				int randNumThree = (int)(Math.random()*(double)fileLength);
+				
 				//ask quiz question
 				System.out.println("What is the "+values[randNum]+" of "+table[randNumThree][randNumTwo]);
 				String input = null;
@@ -52,6 +54,7 @@ public class Main {
 					System.out.println("IO ERROR EXITING PROGRAM");
 					System.exit(1);
 				}
+				
 				//read input, set everything to upper case
 				if(input.toUpperCase().equals(table[randNumThree][randNum].toUpperCase())){
 					System.out.println("Correct!");
